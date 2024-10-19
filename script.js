91d9fe2f4f74c723bf9fcd5e15303912775138fc
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-10-19 13:27:18 -0700

    Add files via upload
    Master Advanced Browser Control: Assign, Replace, and Reload URLs with JavaScript!

---
+++ script.js
@@ -0,0 +1,18 @@
+// Chapter 77: Browser control: Getting and setting the url another way
+
+
+// assign method // can go back
+// window.location.assign("https://www.google.com/search?q=google+classroom&oq=&gs_lcrp=EgZjaHJvbWUqCQgBECMYJxjqAjIJCAAQIxgnGOoCMgkIARAjGCcY6gIyCQgCECMYJxjqAjIJCAMQIxgnGOoCMgkIBBAjGCcY6gIyCQgFECMYJxjqAjIJCAYQIxgnGOoCMgkIBxAjGCcY6gLSAQkyNDMwajBqMTWoAgiwAgE&sourceid=chrome&ie=UTF-8");
+
+
+// replace method  // can't go back
+// window.location.replace("https://www.google.com/search?q=google+classroom&oq=&gs_lcrp=EgZjaHJvbWUqCQgBECMYJxjqAjIJCAAQIxgnGOoCMgkIARAjGCcY6gIyCQgCECMYJxjqAjIJCAMQIxgnGOoCMgkIBBAjGCcY6gIyCQgFECMYJxjqAjIJCAYQIxgnGOoCMgkIBxAjGCcY6gLSAQkyNDMwajBqMTWoAgiwAgE&sourceid=chrome&ie=UTF-8");
+
+// Reloading the current page 
+//    you can reload the current page with:
+
+//    window.location.reload(true); // Reload from server 
+//    window.location.reload(false); // Reload from cache
+//    window.location.reload(); // default: cache
+
+
